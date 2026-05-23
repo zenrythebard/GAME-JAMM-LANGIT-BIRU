775fdfca4dac7fbdafde89695c9f319dc0642d04
zenrythebard <thenajzen@gmail.com>
2026-05-23 10:04:02 +0700

    node

--- script/direction_comp.gd
+++ script/direction_comp.gd
@@ -1,4 +1,11 @@
-class_name direction_comp extends Node2D
+extends Node2D
 
 
-var direction: Vector2
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- script/movement_comp.gd
+++ script/movement_comp.gd
@@ -3,7 +3,6 @@ class_name movement_comp extends Node2D
 @export var speed: float
 @export var body: CharacterBody2D
 var direction: Vector2
-@export var dir_comp: direction_comp
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,12 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	direction  = dir_comp.direction
-	if direction:
-		body.velocity = direction * speed
-		body.move_and_collide(body.velocity * delta)
-		body.move_and_slide()
-	else:
-		body.velocity = body.velocity.move_toward(Vector2.ZERO, delta)
-	
+	body.velocity = direction * speed
+	body.move_and_collide(body.velocity * delta)
+	body.move_and_slide()
 	pass
