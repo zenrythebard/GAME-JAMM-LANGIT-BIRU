fa51b13a30e6452ea53ee614b67a044c3e674c45
LouiPor <louipor22@gmail.com>
2026-05-25 03:42:42 +0700

    henry_added

--- script/enemy_henry.gd
+++ script/enemy_henry.gd
@@ -3,9 +3,10 @@ var dialogue_trigger : bool = false
 @onready var vision_comp: VisionComp = $vision_comp
 const BALLOON = preload("uid://cxfbfdtsbb8fd")
 const TUTORIAL = preload("uid://d2fycmbqheegn")
+const MEETING_YOUNG_HENRY = preload("uid://bk2trrslb14ld")
 
 func _process(delta: float) -> void:
 	if dialogue_trigger == true:
 		return
 	if vision_comp.sighted == true:
-		DialogueManager.show_dialogue_balloon_scene(BALLOON, TUTORIAL)
+		DialogueManager.show_dialogue_balloon_scene(BALLOON, MEETING_YOUNG_HENRY)
