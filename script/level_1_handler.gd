b9369b3b9e17d7e8e87788053f3a4d7c0bd6eeae
LouiPor <louipor22@gmail.com>
2026-05-24 20:14:54 +0700

    prog67

--- script/level_1_handler.gd
+++ script/level_1_handler.gd
@@ -1,9 +1,11 @@
 extends Node2D
 const BALLOON = preload("uid://cxfbfdtsbb8fd")
 const TUTORIAL = preload("uid://d2fycmbqheegn")
+@onready var canvas_modulate: CanvasModulate = $CanvasModulate
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	canvas_modulate.visible = true
 	DialogueManager.show_dialogue_balloon_scene(BALLOON, TUTORIAL)
 
 
