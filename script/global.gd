505999e6fb3608d927bbede491ef2d61857079df
zenrythebard <thenajzen@gmail.com>
2026-05-25 02:09:52 +0700

    Bugfix 2

--- script/global.gd
+++ script/global.gd
@@ -1,3 +1,4 @@
 extends Node
-
+const lvl1 = "uid://wcnmegsad67h"
 var level_age : float = 0
+var load_level = lvl1
--- script/state_comp.gd
+++ script/state_comp.gd
@@ -2,6 +2,7 @@ class_name StateComp extends Node2D
 @export var body:CharacterBody2D
 var state:String
 @export var direction_comp: DirectionComp
+var prev_state : String
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,16 +10,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	prev_state = state
 	if state == "hurt":
 		return
 	if state == "dashing":
 		return
-	if direction_comp.attacking == true :
-		if  direction_comp.direction.y > 0:
-			state = "attacking_down"
-			return
-		else :
-			state = "attacking"
+	if direction_comp.attacking == true:
+		state = "attacking"
 		return
 	
 	if body.velocity:
